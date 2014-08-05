4e4d29d1dc6754b77195fbfbd236847c45f47015
Shea Craig <shea.craig@da.org>
2014-08-05 14:52:33 -0400

    Fix path issues as pointed out by @DeliciousNukaCola.
    os.chdir() can't change to an empty string value, which is what was
    being returned under certain circumstances.
    
    Instead, training program has been updated make a note of the location
    of the module at runtime, used to figure out where its support files are
    located.
    
    Also, it now writes its output to the current directory rather than the
    module's directory, which is probably what you wanted when you ran it
    from outside of the project directory in the first place.

--- ExampleProgram.py
+++ ExampleProgram.py
@@ -2,20 +2,21 @@
 # Generate a 531 program for our CrossFit Total PR Challenge
 # Uses the First Set Last assistance program
 
-import TrainingProgram as TP
 import os
-ospjdf = os.path.join(os.path.dirname(__file__))
-os.chdir(ospjdf)
+
+import TrainingProgram as TP
+
+PROJECT_DIR = os.path.dirname(__file__)
 
 def main():
     tp = TP.TrainingProgram()
 
     # These are my minor assistance exercises
-    work = {'Squat': '', 'Deadlift': '', 
+    work = {'Squat': '', 'Deadlift': '',
             'Press': 'Pullup 5 x 10', 'Bench Press': 'DB Row 5 x 10'}
 
     # These are my abz and stuff
-    extra_work = {'Squat': 'Abs 5 x 10-20', 'Deadlift': 'Abs 5 x 10-20', 
+    extra_work = {'Squat': 'Abs 5 x 10-20', 'Deadlift': 'Abs 5 x 10-20',
             'Press': 'Kurlz 5 x 10', 'Bench Press': 'Tricepz 5 x 10'}
 
     # Specify assistance
@@ -25,6 +26,8 @@ def main():
     assistance.append([[TP.generate_assistance_assistance, extra_work]])
 
     tp.generate_training_cycle((1, 2, 3, 'X', 1, 2, 3, 'X', 1, 2), assistance)
+    tp.add_training_notes(os.path.join(PROJECT_DIR, 'notes.txt'))
+    tp.add_training_notes(os.path.join(PROJECT_DIR, 'unicorn.txt'))
     tp.print_training_cycle()
     tp.write_training_plan()
 
